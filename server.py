97e5534283acf34420200a657d06ad17b57a91bf
Vlad <seven712234234@gmail.com>
2026-07-17 22:35:48 +0300

    added html and some logic

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
+import pathlib
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 latest_status_string = "Waiting for data from sensor..."
 
-def format_sensor_data(data: dict):
-    result = ""
-    for sensor_name, sensor_data in data.items():
-        result += f"{sensor_data}\n"
+def format_data(data: dict):
+    result = []
+    for sensor_name in data:
+        result.append("\n".join(data[sensor_name]))
     return result
 
 @app.post("/api/weather")
@@ -19,7 +20,14 @@ async def receive_weather_data(request: Request):
         latest_status = {"error": f"Invalid JSON: {e}"}
     return {"status": "success"}
 
-@app.get("/")
+@app.get("/", response_class=PlainTextResponse)
 async def get_weather_page():
-    headers = {"Refresh": "1", "Content-Type": "application/json; charset=utf-8"}
-    return JSONResponse(content=format_sensor_data(latest_status), headers=headers)
+    if "error" in latest_status:
+        body = latest_status["error"]
+    elif latest_status.get("message"):
+        body = latest_status["message"]
+    else:
+        body = format_data(latest_status)
+
+    headers = {"Refresh": "1"}
+    return PlainTextResponse(content=body, headers=headers)
