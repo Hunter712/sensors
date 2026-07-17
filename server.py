d79e41b780033d2aefcb9e32941785125a2c2769
Vlad <seven712234234@gmail.com>
2026-07-17 23:38:48 +0300

    changed server logic

--- server.py
+++ server.py
@@ -6,17 +6,6 @@ app = FastAPI()
 latest_status = "Waiting for data from sensor..."
 TEMPLATE = pathlib.Path("templates/index.html").read_text(encoding="utf-8")
 
-def build_html(data: dict):
-    cards = []
-    for sensor_name, sensor_data in data.items():
-        parts = [f"<b>{sensor_name}</b>"]
-        for metric_name, value in sensor_data.items():
-            parts.append(f"{metric_name}: {value}")
-
-        cards.append("<p>" + "<br>".join(parts) + "</p>")
-
-    return "\n".join(cards)
-
 @app.post("/api/weather")
 async def receive_weather_data(request: Request):
     global latest_status
@@ -26,13 +15,10 @@ async def receive_weather_data(request: Request):
         latest_status = {"error": f"Invalid JSON: {e}"}
     return {"status": "success"}
 
+@app.get("/api/weather")
+async def get_weather_data():
+    return latest_status
+
 @app.get("/", response_class=HTMLResponse)
 async def get_weather_page():
-    if "error" in latest_status:
-        body = latest_status["error"]
-    elif latest_status.get("message"):
-        body = latest_status["message"]
-    else:
-        body = build_html(latest_status)
-
-    return HTMLResponse(content=TEMPLATE.replace("{body}", body))
+    return HTMLResponse(content=TEMPLATE)
