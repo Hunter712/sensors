7189e50c44cbeadc047cc7e4ce9e4c0495b8e540
Vlad <seven712234234@gmail.com>
2026-07-17 23:48:58 +0300

    changed server logic

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, PlainTextResponse
 
 app = FastAPI()
-latest_status = "Waiting for data from sensor..."
+latest_status = {"message": "Waiting for data from sensor..."}
 TEMPLATE = pathlib.Path("templates/index.html").read_text(encoding="utf-8")
 
 def build_html(data: dict):
